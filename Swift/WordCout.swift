ccf5b8a3c1240dc33d928037810be0c4e2d8f97d
Shady Ahmed <44348962+shady-abdelaal@users.noreply.github.com>
2020-01-09 14:45:43 +0100

    Create WordCout.swift

---
+++ Swift/WordCout.swift
@@ -0,0 +1,35 @@
+import Foundation
+var full_text = "This is a test This is"
+full_text = full_text.lowercased()
+let full_text_array = full_text.components(separatedBy: " ")
+
+var dictionary_count: [String: Int] = [:]
+
+for word in full_text_array {
+  if  let val = dictionary_count[word] {
+    dictionary_count[word] = val + 1
+  }
+  else {
+    dictionary_count[word] = 1
+  }
+}
+
+let byValue = {
+  (elem1:(key: String, val: Int), elem2:(key: String, val: Int))->Bool in
+  if elem1.val > elem2.val {
+    return true
+  } else {
+    return false
+  }
+}
+let sortedDict = dictionary_count.sorted(by: byValue)
+ let no_of_words = dictionary_count.count
+
+print("\(no_of_words) Words")
+var index_count = 1
+for(key,value) in sortedDict {
+  print("\(index_count). \(key) - \(value)")
+  index_count += 1
+}
+//print(full_text_array.count)
+//print(sorted_dictionary_count[0])
